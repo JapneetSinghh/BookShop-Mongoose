9a705ae307f21e040d2a88e08403cf3f718eb462
Japneet Singh Baluja <96378977+JapneetSinghh@users.noreply.github.com>
2023-01-02 13:42:48 +0530

    Add files via upload

--- controller/admin.js
+++ controller/admin.js
@@ -1,14 +1,20 @@
 const Product = require('../models/product');
-const mongodb = require('mongodb');
+const { validationResult } = require('express-validator');
+// const mongodb = require('mongodb');
+const User = require('../models/user');
 exports.getAddProudct = (req, res, next) => {
-  Product.fetchAll().then(products => {
-    res.render('admin/add-product', {
-      pageTitle: 'Add Product',
-      path: '/add-product',
-      products: products,
-      editMode: false
+  Product.find()
+    .then(products => {
+      res.render('admin/add-product', {
+        pageTitle: 'Add Product',
+        path: '/add-product',
+        products: products,
+        editMode: false,
+        hasError: false,
+        message: null,
+        validation: 'error'
+      });
     });
-  });
 }
 
 exports.postAddProducts = (req, res, next) => {
@@ -16,14 +22,56 @@ exports.postAddProducts = (req, res, next) => {
   const prodPrice = req.body.productPrice;
   const prodDescription = req.body.productDescription;
   const prodImage = req.body.productLink;
+  const productBy = req.session.user.username;
   const prodId = null;
-  const product = new Product(prodName, prodPrice, prodImage, prodDescription,null,req.user._id);
-  product.save();
-  res.redirect('/add-product');
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.render('admin/add-product', {
+      pageTitle: 'Add Product',
+      path: '/add-product',
+      editMode: false,
+      hasError: true,
+      products: {
+        name: prodName,
+        price: prodPrice,
+        description: prodDescription,
+        imageUrl: prodImage
+      },
+      message: errors.array()[0].msg,
+      validation: 'error'
+
+    });
+  }
+
+  const product = new Product({
+    name: prodName,
+    price: prodPrice,
+    description: prodDescription,
+    imageUrl: prodImage,
+    userId: req.user._id,
+    productBy: productBy
+  });
+  product.save().then(result => {
+    res.render('admin/add-product', {
+      pageTitle: 'Add Product',
+      path: '/add-product',
+      editMode: false,
+      hasError: true,
+      products: {
+        name: '',
+        price: '',
+        description: '',
+        imageUrl: ''
+      },
+      message: `${prodName} Added Successfully`,
+      validation: 'success'
+    });
+  }); // MONGOOSE FUNCTION
   console.log("Someone added new product");
 }
 exports.getAdmin = (req, res, next) => {
-  Product.fetchAll()
+  Product.find({ userId: req.user._id })
     .then(products => {
       console.log('--- ADMIN ---');
       // console.log(products);
@@ -49,25 +97,104 @@ exports.getEditProduct = (req, res, next) => {
         pageTitle: 'Edit Product',
         path: '',
         products: product,
-        editMode: true
+        editMode: true,
+        hasError: false,
+        message: null,
+        validation: 'error'
       })
     })
   }
 }
 
 exports.postEditProduct = (req, res, next) => {
+
   const prodName = req.body.productName;
   const prodPrice = req.body.productPrice;
   const prodId = req.body.productId;
   const prodDescription = req.body.productDescription;
   const prodImage = req.body.productLink;
-  const product = new Product(prodName, prodPrice, prodImage, prodDescription,new mongodb.ObjectId(prodId),req.user._id);
-  product.save();
-  res.redirect('/');
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.render('admin/add-product', {
+      pageTitle: 'Edit Product',
+      path: '/add-product',
+      editMode: true,
+      hasError: true,
+      products: {
+        name: prodName,
+        price: prodPrice,
+        description: prodDescription,
+        imageUrl: prodImage
+      },
+      message: errors.array()[0].msg,
+      validation: 'error'
+    });
+  }
+  Product.findById(prodId)
+    .then(product => {
+      if (product.userId.toString() !== req.user._id.toString()) {
+        req.flash('error', 'Unauthorized Access');
+        return (res.redirect('/login'));
+      }
+      console.log(product);
+      product.name = prodName;
+      product.price = prodPrice;
+      product.imageUrl = prodImage;
+      product.description = prodDescription;
+      product.save();
+    })
+    .then(result => {
+      return res.render('admin/add-product', {
+        pageTitle: 'Edit Product',
+        path: '/add-product',
+        editMode: true,
+        hasError: true,
+        products: {
+          name: '',
+          price: '',
+          description: '',
+          imageUrl: ''
+        },
+        message: 'Product Updated Successfully',
+        validation: 'success'
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 }
 exports.deleteProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  console.log('DELETING PRODUCT USING PRODUCT ID: '+prodId);
-  Product.delById(prodId);
-  res.redirect('/admin');
+  console.log('DELETING PRODUCT USING PRODUCT ID: ' + prodId);
+  Product.deleteOne({ _id: prodId, userId: req.user.id })
+    .then(result => {
+      // Deleting from current users cart
+      req.user.delFromCart(prodId)
+        .then(result => {
+          console.log(result);
+        })
+        .catch(err => {
+        })
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  User.find()
+    .then(users => {
+      users.forEach(user => {
+        user.delFromCart(prodId)
+          .then(result => {
+          })
+          .catch(err => {
+            console.log(err);
+          })
+      })
+    })
+    .then(result => {
+      res.redirect('/admin')
+    })
+    .catch(err => {
+      console.log(err);
+    })
 }
--- routers/shop.js
+++ routers/shop.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const shopController=require('../controller/shop');
+const isAuth = require('../Middlewear/isAuth');
 
 router.get('/',shopController.getProducts);
-router.get('/cart',shopController.getCart);
-router.post('/addToCart',shopController.getAddToCart);
-router.post('/removeFromCart',shopController.getRemoveFromCart);
+router.get('/cart',isAuth,shopController.getCart);
+router.post('/addToCart',isAuth,shopController.getAddToCart);
+router.post('/removeFromCart',isAuth,shopController.getRemoveFromCart);
 router.get('/getDetails/:productId',shopController.getDetails);
-router.post('/orderNow',shopController.addOrder);
-router.get('/orders',shopController.getOrders);
+router.post('/orderNow',isAuth,shopController.postOrder);
+router.get('/orders',isAuth,shopController.getOrder);
 exports.routes = router;
